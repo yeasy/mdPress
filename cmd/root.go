c4eb26594f3b4b496f840fc0105e33bc196ddbd5
yeasy <yangbaohua@gmail.com>
2026-03-27 09:35:26 -0700

    Update changelog for v0.6.6

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	// Version is overridden at build time via -ldflags.
-	Version = "0.6.5"
+	Version = "0.6.6"
 	// BuildTime is overridden at build time via -ldflags.
 	BuildTime = "unknown"
 	// rootCmd is the root command for the mdpress application.
