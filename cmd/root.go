05149e220e4b5cb4f617a32a7f05f13229c062a4
yeasy <yangbaohua@gmail.com>
2026-03-30 12:58:57 -0700

    Bump version to v0.7.2

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	// Version is overridden at build time via -ldflags.
-	Version = "0.7.1"
+	Version = "0.7.2"
 	// BuildTime is overridden at build time via -ldflags.
 	BuildTime = "unknown"
 	// rootCmd is the root command for the mdpress application.
